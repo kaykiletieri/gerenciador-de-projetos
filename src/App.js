888a1f53f7b41c38819a537be7315b7b5f2e6dcf
Kayki Letieri <54692628+Kayki7@users.noreply.github.com>
2022-06-01 16:57:43 -0300

    Update Navbar and Footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
+
 import Home from './components/pages/Home'
-import Contact from './components/pages/Contact'
+import Projects from './components/pages/Projects'
 import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
+
+import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
         <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/contact' element={<Contact />}/>
-        <Route path='/company' element={<Company />}/>
-        <Route path='/new-project' element={<NewProject />}/>
-      </Routes>
-        
+
+    <Container customClass="min-height" >
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/projects' element={<Projects />}/>           
+            <Route path='/company' element={<Company />}/>
+            <Route path='/contact' element={<Contact />}/>
+            <Route path='/new-project' element={<NewProject />}/>
+          </Routes>
+      </Container>
+
+        <Footer />
     </Router>
   );
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,35 @@
 import { Link } from "react-router-dom";
 
+import Container from "./Container";
+
+import styles from "./Navbar.module.css";
+import logo from "../../img/costs_logo.png";
+
 function Navbar() {
-    return  <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/contact'>Contato</Link>
-                <Link to='/company'>Empresa</Link>
-                <Link to='/new-project'>Novo Projeto</Link>
+    return  <nav className={styles.navbar}>
+                <Container>
+                    <Link to='/'>
+                        <img src={logo} alt="logo"></img>
+                    </Link>
+                    <ul className={styles.list}>
+                        <li className={styles.item}>
+                            <Link to='/'>Home</Link>
+                        </li>
+
+                        <li className={styles.item}>
+                            <Link to='/projects'>Projetos</Link>
+                        </li>
+
+
+                        <li className={styles.item}>
+                            <Link to='/company'>Empresa</Link>
+                        </li>
+
+                        <li className={styles.item}>
+                            <Link to='/contact'>Contato</Link>
+                        </li>
+                    </ul>             
+                </Container>
             </nav>
 }
 
