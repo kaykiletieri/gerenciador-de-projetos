533687bcffd59e350bffa8f88e7a4aa43877847d
Kayki Letieri <54692628+Kayki7@users.noreply.github.com>
2022-06-01 21:00:33 -0300

    Update Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,18 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>Gerenciador de Projetos</span></h1>
+            <p>Começe a gerenciar seus projetos agora mesmo!</p>
+
+            <LinkButton to="/new-project" text="Criar Projeto" />
+
+            <img src={savings} alt="img-home"></img>
+        </section>
+    )
 }
 
 export default Home
