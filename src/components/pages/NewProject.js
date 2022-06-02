4237875098a9ae5cc309559767f6a9e6a07f4ad5
Kayki Letieri <54692628+Kayki7@users.noreply.github.com>
2022-06-01 22:16:36 -0300

    Update

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,16 @@
+import styles from './NewProject.module.css'
+
+import ProjectFom from '../projects/ProjectForm'
+
 function NewProject() {
-    return <h1>NewProject</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            
+            <ProjectFom />
+        </div>
+    )
 }
 
 export default NewProject
