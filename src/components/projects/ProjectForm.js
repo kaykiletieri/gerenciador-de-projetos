2d7caac37dcd85d4f7077cc3399122c765f8e8fc
Kayki Letieri <54692628+kaykiletieri@users.noreply.github.com>
2022-06-02 19:10:03 -0300

    refact form

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,26 +1,29 @@
 import styles from './ProjectForm.module.css'
 
-function ProjectFom () {
+import Input from '../form/Input'/*
+import Select from '../form/Select'*/
+import SubmitButton from '../form/SubmitButton'
+
+function ProjectFom ({btntext}) {
  return (
      <form className={styles.form}>
-        <div>
-            <input type="text" placeholder="Insira o nome do projeto"/>
-        </div>
 
-        <div>
-            <input type="number" placeholder="Insira o orçamento total"/>
-        </div>
+        <Input type="text" text="Nome do Projeto" name="name" placeholder="Insira o nome do projeto"/>
 
-        <div>
-            <select name="category_id">
-                <option>Selecione a categoria</option> 
-            </select>
-        </div>
+        <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total"/>
 
-        <div>
-            <input type="submit" value="Criar projeto" />
-        </div>
+
+        <SubmitButton text={btntext}/>
      </form>
+
+     /* dentro de form
+     
+        <Select 
+        name="category_id"
+        text="Selecione a categoria"
+        />
+     
+     */
  )
 }
 
