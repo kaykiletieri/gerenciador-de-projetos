74fa68cbf79c91f00b8f41ff25860f374db19903
Kayki Letieri <54692628+kaykiletieri@users.noreply.github.com>
2022-06-03 21:40:06 -0300

    feat: backend

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -7,9 +7,15 @@ function Select({ text, name, options, handleOnChange, value }) {
       <select
         name={name}
         id={name}
+        onChange={handleOnChange}
+        value={value || ''}
       >
         <option>Selecione uma opção</option>
-        
+        {options.map((options) => (
+          <option value={options.id} key={options.id}>
+            {options.name}
+          </option>
+        ))}
       </select>
     </div>
   )
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,14 +1,39 @@
+import { useNavigate } from 'react-router-dom'
+
 import styles from './NewProject.module.css'
 
 import ProjectFom from '../projects/ProjectForm'
 
 function NewProject() {
+
+    const navigate = useNavigate()
+
+    function createPost(project) {
+
+        // initialize project-manager and services
+        project.pm = 0
+        project.services = []
+
+        fetch("http:/localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            // redirect
+        })
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
             
-            <ProjectFom btntext="Criar Projeto" />
+            <ProjectFom handleSubmit={createPost} btnText="Criar Projeto" />
         </div>
     )
 }
