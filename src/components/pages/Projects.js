3a8167b0fba308545b17ad975a4e24cbd3e68fc8
Kayki Letieri <54692628+kaykiletieri@users.noreply.github.com>
2022-06-08 19:05:41 -0300

    feat: created loader component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 
 import Message from "../layout/Message"
 import Container from "../layout/Container"
+import Loading from "../layout/Loading"
 import LinkButton from "../layout/LinkButton"
 import ProjectCard from "../projects/ProjectCard"
 
@@ -12,6 +13,7 @@ import styles from './Projects.module.css'
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -20,7 +22,7 @@ function Projects() {
     }
 
     useEffect(()=>{
-
+        setTimeout(()=>{
         fetch('http://localhost:5000/projects',
         {
             method: 'GET',
@@ -31,8 +33,10 @@ function Projects() {
         .then(data => {
            // console.log(data)
             setProjects(data)
+            setRemoveLoading(true)
         })
         .catch((err) => console.log(err))
+        },250)
     },[])
 
     return (
@@ -52,7 +56,10 @@ function Projects() {
                     category={project.category.name}
                     key={project.id}
                 /> )}
-
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
