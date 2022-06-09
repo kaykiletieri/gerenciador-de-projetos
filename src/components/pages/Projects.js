0df3921c9729159d93fcdf106ef9c051b3026e02
Kayki Letieri <54692628+kaykiletieri@users.noreply.github.com>
2022-06-08 21:56:20 -0300

    feat: added delete projects function

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -14,6 +14,7 @@ function Projects() {
 
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -39,6 +40,20 @@ function Projects() {
         },250)
     },[])
 
+    function removeProject (id){
+        fetch(`https://localhost/5000/projects/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(()=>{
+            setProjects(projects.filter((project)=> project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(err=> console.log(err))
+    }
+
     return (
         <div className={styles.project_container}>   
             <div className={styles.title_container}>
@@ -46,6 +61,7 @@ function Projects() {
                 <LinkButton to="/new-project" text="Criar Projeto" />
             </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={message} />}
 
             <Container customClass="start">
 
@@ -55,6 +71,7 @@ function Projects() {
                     budget={project.budget}
                     category={project.category.name}
                     key={project.id}
+                    handleRemove={removeProject}
                 /> )}
                 {!removeLoading && <Loading />}
                 {removeLoading && projects.length === 0 && (
