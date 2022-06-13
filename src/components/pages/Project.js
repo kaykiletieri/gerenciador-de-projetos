66f15849da1e936b566302591216d965f33f0cc3
Kayki Letieri <54692628+kaykiletieri@users.noreply.github.com>
2022-06-12 23:38:44 -0300

    feat: added a service area

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -13,6 +13,7 @@ function Project() {
     
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState('')
     const [type, setType] = useState('success')
 
@@ -31,7 +32,9 @@ function Project() {
         },300)
     }, [id])
 
+
     function editPost(project) {
+        setMessage('')
         // budget validation
         if (project.budget < project.cost) {
           setMessage('O Orçamento não pode ser menor que o custo do projeto!')
@@ -57,6 +60,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -91,6 +98,25 @@ function Project() {
                             </div>
                         )}
                     </div>
+
+                    <div className={styles.service_from_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm && (
+                                    <div>
+                                        Formulário serviços
+                                    </div>
+                                )
+                                }
+                            </div>
+                    </div>
+                    <h2>Serviços</h2>
+                    <Container customClass="start">
+                        <p>Itens de serviços</p>
+                    </Container>
                 </Container>
             </div>
         ):(<Loading />)}</>
