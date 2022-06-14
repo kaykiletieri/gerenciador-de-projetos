ef98045d4c9694903d7c3dfd3efc32403bdc7922
Kayki Letieri <54692628+kaykiletieri@users.noreply.github.com>
2022-06-14 00:34:24 -0300

    fixed: added function to delete services

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -104,9 +104,30 @@ function Project() {
 
     }
 
-    function removeService() {
-
-    }
+    function removeService(id, cost) {
+        const servicesUpdated = project.services.filter(
+          (service) => service.id !== id,
+        )
+    
+        const projectUpdated = project
+    
+        projectUpdated.services = servicesUpdated
+        projectUpdated.pm = parseFloat(projectUpdated.pm) - parseFloat(cost)
+    
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(projectUpdated),
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            setProject(projectUpdated)
+            setServices(servicesUpdated)
+            setMessage('Serviço removido com sucesso!')
+          })
+      }
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
